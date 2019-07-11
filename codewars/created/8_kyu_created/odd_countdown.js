a6af48b69bb459d53251d6aef742f71a4f601993
ctt1988 <clarence.tooles@gmail.com>
2019-07-10 20:06:45 -0400

    folder change

---
+++ codewars/created/8_kyu_created/odd_countdown.js
@@ -0,0 +1,6 @@
+//Find how many odd number are underneath the number given
+
+function oddCount
+
+//If number given equals 5, expect result to be 2
+//If number given equas 10, expect result to be 5
